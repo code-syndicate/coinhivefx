33fe509bbc566c0570a0b4b14724f0ee87250fe8
timmy-oss <timileyinpelumi.dev@gmail.com>
2022-08-21 18:16:29 +0100

    :?new

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,87 +1,90 @@
-const User1 = require('./../models/user');
-const {Deposit1, Withdrawal1, AuthPin1} = require('./../models/transaction');
-const Notification1 = require('./../models/notification');
-const {body, validationResult} = require('express-validator');
+const User1 = require("./../models/user");
+const { Deposit1, Withdrawal1, AuthPin1 } = require("./../models/transaction");
+const Notification1 = require("./../models/notification");
+const { body, validationResult } = require("express-validator");
 
 function logIn(req, res) {
-	res.locals.authError = req.flash('error');
-	res.locals.formErrors = req.flash('formErrors');
-	res.render('admin_login');
+  res.locals.authError = req.flash("error");
+  res.locals.formErrors = req.flash("formErrors");
+  res.render("admin_login");
 }
 
 async function deleteUser(req, res) {
-	const id = req.params.clientId;
-	await Withdrawal1.deleteMany({client: id}).exec();
-	await Deposit1.deleteMany({client: id}).exec();
-	await Notification1.deleteMany({listener: id}).exec();
-	await AuthPin1.deleteMany({client: id}).exec();
+  const id = req.params.clientId;
+  await Withdrawal1.deleteMany({ client: id }).exec();
+  await Deposit1.deleteMany({ client: id }).exec();
+  await Notification1.deleteMany({ listener: id }).exec();
+  await AuthPin1.deleteMany({ client: id }).exec();
 
-	await User1.findByIdAndDelete(id).exec();
+  await User1.findByIdAndDelete(id).exec();
 
-	res.redirect('/admin/overview/?ui=users');
+  res.redirect("/admin/overview/?ui=users");
 }
 
 async function overview(req, res) {
-	let UI = req.query.ui || 'main';
+  let UI = req.query.ui || "main";
 
-	const uis = ['main', 'users', 'deposits', 'withdrawals'];
+  const uis = ["main", "users", "deposits", "withdrawals"];
 
-	if (!uis.includes(UI)) UI = 'main';
+  if (!uis.includes(UI)) UI = "main";
 
-	const clients = await User1.find({}).exec();
-	const deposits = await Deposit1.find({}).populate('client').exec();
-	const withdrawals = await Withdrawal1.find({}).populate('client').exec();
+  const clients = await User1.find({}).exec();
+  const deposits = await Deposit1.find({}).populate("client").exec();
+  const withdrawals = await Withdrawal1.find({}).populate("client").exec();
 
-	res.locals.authError = req.flash('error');
-	res.locals.formErrors = req.flash('formErrors');
+  res.locals.authError = req.flash("error");
+  res.locals.formErrors = req.flash("formErrors");
 
-	res.render('admin_overview', {
-		clients,
-		deposits,
-		withdrawals,
-		ui: UI,
-		user: req.user,
-	});
+  res.render("admin_overview", {
+    clients,
+    deposits,
+    withdrawals,
+    ui: UI,
+    user: req.user,
+  });
 }
 
 const editClient = [
-	body('wallet', 'Wallet balance is required')
-		.notEmpty()
-		.isNumeric()
-		.withMessage('Please enter a valid wallet amount'),
-	body('bonus', 'Bonus balance is required')
-		.notEmpty()
-		.isNumeric()
-		.withMessage('Please enter a valid bonus amount'),
-	body('profit', 'Profit balance is required')
-		.notEmpty()
-		.isNumeric()
-		.withMessage('Please enter a valid profit amount'),
-	async function (req, res) {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			req.flash('formErrors', errors.array());
-			res.redirect('/admin/overview/?ui=users');
-			return;
-		}
+  body("wallet", "Wallet balance is required")
+    .notEmpty()
+    .isNumeric()
+    .withMessage("Please enter a valid wallet amount"),
+  body("bonus", "Bonus balance is required")
+    .notEmpty()
+    .isNumeric()
+    .withMessage("Please enter a valid bonus amount"),
+  body("deposits", "Deposit balance is required")
+    .notEmpty()
+    .isNumeric()
+    .withMessage("Please enter a valid amount"),
+  body("withdrawals", "Withdrawal balance is required")
+    .notEmpty()
+    .isNumeric()
+    .withMessage("Please enter a valid  amount"),
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.flash("formErrors", errors.array());
+      res.redirect("/admin/overview/?ui=users");
+      return;
+    }
 
-		let client = await User1.findById(req.body.clientId).exec();
-		client.wallet = req.body.wallet;
-		client.bonus = req.body.bonus;
-		client.profits = req.body.profit;
+    let client = await User1.findById(req.body.clientId).exec();
+    client.wallet = req.body.wallet;
+    client.bonus = req.body.bonus;
+    client.deposits = req.body.deposits;
+    client.withdrawals = req.body.withdrawals;
 
-		await client.save();
-		req.flash(
-			'info',
-			` Client ${client.email} record updated successfully`
-		);
-		res.redirect('/admin/overview/?ui=users');
-	},
+
+    await client.save();
+    req.flash("info", ` Client ${client.email} record updated successfully`);
+    res.redirect("/admin/overview/?ui=users");
+  },
 ];
 
 module.exports = {
-	logIn,
-	overview,
-	editClient,
-	deleteUser,
+  logIn,
+  overview,
+  editClient,
+  deleteUser,
 };
